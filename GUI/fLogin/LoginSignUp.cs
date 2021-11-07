d3bbc9ac01c268a65686079befc58d963b55224d
Tuan <20522122@gm.uit.edu.vn>
2021-11-07 17:56:49 +0700

    add registration function and few things

--- GUI/fLogin/LoginSignUp.cs
+++ GUI/fLogin/LoginSignUp.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BUS;
 
 namespace GUI.fLogin
 {
@@ -21,6 +22,31 @@ namespace GUI.fLogin
         {
             Hide();
             Login.Instance.Show();
+        }
+
+        private void fLogin_LoginSignUp_btn_creAcc_Click(object sender, EventArgs e)
+        {
+            string MSSV = fLogin_LoginSignUp_txb_mssv.Text;
+            string username = fLogin_LoginSignUp_txb_username.Text;
+            string password = fLogin_LoginSignUp_txb_password.Text;
+            if(Login_BUS.Instance.LoginSignUp_Check(MSSV, username, password))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (Login_BUS.Instance.LoginSignUp_CheckMSSVExitst(MSSV))
+                    MessageBox.Show("MSSV đã tồn tại", "Thông báo",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    Login_BUS.Instance.InsertAcc(MSSV, username, password);
+                    MessageBox.Show("Đăng kí thành công", "Thông báo",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } 
+            }
+
         }
     }
 }
